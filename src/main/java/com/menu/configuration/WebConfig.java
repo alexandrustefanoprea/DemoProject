57b063d3ddad7cf73740418c81fdd836ab35a50b
alexandrustefanoprea <opreastefan11@stud.ase.ro>
2016-01-14 00:52:38 +0200

    Added configuration
    Added annotation based configuration

---
+++ src/main/java/com/menu/configuration/WebConfig.java
@@ -0,0 +1,21 @@
+package com.menu.configuration;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig  {
+	
+
+}
